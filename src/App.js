a436f0810aff53b94f2a188a599c88470fc9b12a
DwkLoki <dwiky.darmawansyah@gmail.com>
2021-04-16 10:33:47 +0700

    add filter and showExcel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-function App() {
+import ShowExcel from "./ShowExcel"
+
+const App = () => {
   return (
-    <div className="App">
-      <h2>Hello world</h2>
+    <div className="container">
+      <h1 className="text-center">Project Third Task Group</h1>
+      <hr />
+      <ShowExcel />
     </div>
   );
 }
---
+++ src/ShowExcel.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react"
+import XLSX from "xlsx"
+import Filter from './Filter'
+
+const ShowExcel = () => {
+
+  const [file, setFile] = useState(null)
+  const [dataset, setDataset] = useState(null)
+
+  const filePreview = (e) => {
+    const files = e.target.files;
+    files && files[0] && setFile(files[0]);
+
+    if (files && files[0]) {
+      /* Boilerplate to set up FileReader */
+      const reader = new FileReader();
+      const rABS = !!reader.readAsBinaryString;
+
+      reader.onload = (e) => {
+        // setLoadPreview(true);
+        /* Parse data */
+        const bstr = e.target.result;
+        const wb = XLSX.read(bstr, {
+          type: rABS ? "binary" : "array",
+          bookVBA: true,
+        });
+
+        /* Get first worksheet */
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+
+        /* Convert array of arrays */
+        const data = XLSX.utils.sheet_to_json(ws);
+        console.log(data);
+
+        setTimeout(() => {
+          setDataset(data);
+          // setLoadPreview(false);
+        }, 500);
+      };
+
+      if (rABS) {
+        reader.readAsBinaryString(files[0]);
+      } else {
+        reader.readAsArrayBuffer(files[0]);
+      }
+    }
+  };
+  
+  return (
+    <div>
+      <h2>Proses Excel Reader</h2>
+      <form>
+        <input type="file" onChange={filePreview}></input>
+      </form>
+      <hr />
+      <Filter dataset={dataset} />
+    </div>
+  )
+}
+
+export default ShowExcel
