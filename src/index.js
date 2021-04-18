b6363ad6361bafd9bbf5585e21ed118501947608
DwkLoki <dwiky.darmawansyah@gmail.com>
2021-04-18 23:21:41 +0700

    selesai membuat komponen redux di index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+
+const globalState = {
+  data: [12, 4, 21, 6, 9, 17],
+  label: ['Merah', 'Biru', 'Kuning', 'Hijau', 'Ungu', 'Orens']
+}
+
+const rootReducer = ( state = globalState, action) => {
+  return state
+}
+
+const globalStore = createStore(rootReducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={globalStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
